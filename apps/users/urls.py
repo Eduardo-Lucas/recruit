9c256ce03da4f90cabfbde9eb1659596b983e989
Eduardo-Lucas <eduardolucas40gmail.com>
2020-09-10 21:02:57 -0300

    speech recognition test has successfully passed so far

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from apps.users.views import jobseekers, employers
 from apps.users.views.employers import EmployerUpdateView
-from apps.users.views.jobseekers import JobSeekerUpdateView
+from apps.users.views.jobseekers import JobSeekerUpdateView, convert_audio_text, audio_text_convertion
 from apps.users.views.users import SignUpView
 
 
@@ -12,6 +12,9 @@ urlpatterns = [
     path('accounts/signup/employer/', employers.EmployerSignUpView.as_view(), name='employer_signup'),
 
     path('edit_jobseeker/<int:pk>', JobSeekerUpdateView.as_view(), name='edit_jobseeker'),
-    path('edit_employer/<int:pk>', EmployerUpdateView.as_view(), name='edit_employer')
+    path('edit_employer/<int:pk>', EmployerUpdateView.as_view(), name='edit_employer'),
+
+    path('convert_audio_text', convert_audio_text, name='convert_audio_text'),
+    path('audio_text_convertion', audio_text_convertion, name='audio_text_convertion'),
 
 ]
--- apps/users/views/jobseekers.py
+++ apps/users/views/jobseekers.py
@@ -1,8 +1,9 @@
 from allauth.account.views import login
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
@@ -32,3 +33,30 @@ class JobSeekerUpdateView(LoginRequiredMixin, UpdateView):
     form_class = JobSeekerForm
     template_name = 'users/jobseeker_form.html'
     success_url = reverse_lazy('home')
+
+
+def convert_audio_text(request):
+    return render(request, 'users/convert_audio_text.html', {})
+
+
+def audio_text_convertion(request):
+    # importing the module
+    import speech_recognition as sr
+    # define the recognizer
+    r = sr.Recognizer()
+    # define the audio file
+    audio_file = sr.AudioFile('audios/116-288045-0004.flac')
+    # speech recognition
+    with audio_file as source:
+        r.adjust_for_ambient_noise(source)
+        audio = r.record(source)
+    result = r.recognize_google(audio)
+    # exporting the result
+    with open('texts/116-288045-0004.txt', mode='w') as file:
+        file.write("Recognized text:")
+        file.write("\n")
+        file.write(result)
+        print("ready!")
+
+    return redirect('convert_audio_text')
+
